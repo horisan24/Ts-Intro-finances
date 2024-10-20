8a1ff353f7f5dcdfb92af4ec4836e5c8ee4ab5ad
horisan24 <horisan24@icloud.com>
2024-10-20 18:43:59 +0900

    first commit

---
+++ src/components/common/SideBar.tsx
@@ -0,0 +1,85 @@
+import { Box, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from '@mui/material'
+import HomeIcon from '@mui/icons-material/Home';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import React from 'react'
+
+interface SideBarProps {
+  drawerWidth: number;
+  mobileOpen: boolean;
+  handleDrawerClose: () => void;
+  handleDrawerTransitionEnd: () => void;
+}
+
+interface SideBarItem {
+  text: string;
+  path: string;
+  icon: React.ComponentType;
+}
+
+const SideBar = ({ drawerWidth, mobileOpen, handleDrawerClose, handleDrawerTransitionEnd }: SideBarProps) => {
+  const drawerItems: SideBarItem[] = [
+    { text: 'Home', path: '/', icon: HomeIcon },
+    { text: 'Report', path: '/report', icon: QueryStatsIcon },
+  ];
+
+  const drawer = (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        {drawerItems.map((item, index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+                <item.icon />
+              </ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </div>
+  );
+
+  return (
+    <Box
+    component="nav"
+    sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    aria-label="mailbox folders"
+  >
+    {/* モバイルメニュー */}
+    <Drawer
+      variant="temporary"
+      open={mobileOpen}
+      onTransitionEnd={handleDrawerTransitionEnd}
+      onClose={handleDrawerClose}
+      ModalProps={{
+        keepMounted: true, // Better open performance on mobile.
+      }}
+      sx={{
+        display: { xs: 'block', sm: 'none' },
+        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+      }}
+    >
+      {drawer}
+    </Drawer>
+
+    {/* PCメニュー */}
+    <Drawer
+      variant="permanent"
+      sx={{
+        display: { xs: 'none', sm: 'block' },
+        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+      }}
+      open
+    >
+      {drawer}
+    </Drawer>
+  </Box>
+
+  )
+}
+
+export default SideBar
