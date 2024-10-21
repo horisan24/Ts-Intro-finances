408d413e8cfe4517eda09e9e7f3c7feb83d1de8e
horisan24 <horisan24@icloud.com>
2024-10-22 01:53:17 +0900

    firebaseと通信

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Home';
@@ -8,7 +8,42 @@ import AppLayout from './components/layout/AppLayout';
 import { theme } from './theme/theme';
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
+import { Transaction } from './types';
+import { getDocs, collection } from 'firebase/firestore';
+import { db } from './firebase';
 function App() {
+
+  // Firestoreエラーかどうかを判断する型ガード
+  function isFireStoreError(err: unknown): err is {code: string, message: string} {
+    return typeof err === "object" && err !== null && "code" in err && "message" in err;
+  }
+
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "Transactions"));
+        // console.log(querySnapshot);
+
+        const transactionsData = querySnapshot.docs.map((doc) => {
+          return {
+            ...doc.data(),
+            id: doc.id,
+          } as Transaction;
+        });
+        // console.log(transactionsData)
+        setTransactions(transactionsData);
+      } catch (err) {
+        if (isFireStoreError(err)) {
+          console.error("Firestoreエラーは:", err);
+        } else {
+          console.error("一般的なエラーは:", err);
+        }
+      }
+    }
+    fetchTransactions();
+  },[]);
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
