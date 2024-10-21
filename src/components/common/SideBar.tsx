be66da64535b81bb62561b60d9fed44b2d99ed3d
horisan24 <horisan24@icloud.com>
2024-10-21 12:00:05 +0900

    月の収支の表示

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -64,7 +64,7 @@ const SideBar = ({ drawerWidth, mobileOpen, handleDrawerClose, handleDrawerTrans
   return (
     <Box
     component="nav"
-    sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    sx={{ width: { md: drawerWidth }, flexShrink: { md: 0 } }}
     aria-label="mailbox folders"
   >
     {/* モバイルメニュー */}
@@ -77,7 +77,7 @@ const SideBar = ({ drawerWidth, mobileOpen, handleDrawerClose, handleDrawerTrans
         keepMounted: true, // Better open performance on mobile.
       }}
       sx={{
-        display: { xs: 'block', sm: 'none' },
+        display: { xs: 'block', md: 'none' },
         '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
       }}
     >
@@ -88,7 +88,7 @@ const SideBar = ({ drawerWidth, mobileOpen, handleDrawerClose, handleDrawerTrans
     <Drawer
       variant="permanent"
       sx={{
-        display: { xs: 'none', sm: 'block' },
+        display: { xs: 'none', md: 'block' },
         '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
       }}
       open
